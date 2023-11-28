d3d232c533009a315ed43cfe0e5b10917f176d7b
Namikazes <egorsitenko35@gmail.com>
2023-11-28 22:59:07 +0200

    [hw-6] Open/Closed-Principle

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-class ProductData {
-    public function get(name) {}
-    public function set(name, value) {}
-}
-
-class productProcessing
-{
-    public function save() {}
-    public function update() {}
-    public function delete() {}
-}
-
-class productOutput
-{
-    public function show() {}
-    public function print() {}
-}
